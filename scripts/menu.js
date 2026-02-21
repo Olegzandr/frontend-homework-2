bf15a6e6f3a6557593324b80315a179e2b6a69f8
afefelov <afefelov@astralinux.ru>
2026-02-21 23:07:52 +0300

    Доработка мобильного интерфейса с гамбургер-меню

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,11 @@
-if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
+// if ('scrollRestoration' in history) history.scrollRestoration = 'manual';
 
-window.addEventListener('load', () => {
-  if (location.hash) {
-    history.replaceState(null, '', location.pathname + location.search);
-  }
-  window.scrollTo(0, 0);
-});
+// window.addEventListener('load', () => {
+//   if (location.hash) {
+//     history.replaceState(null, '', location.pathname + location.search);
+//   }
+//   window.scrollTo(0, 0);
+// });
 
 const menuToggle = document.getElementById('menuToggle');
 const mainNav = document.getElementById('mainNav');
